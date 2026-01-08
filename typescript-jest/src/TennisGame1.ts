c78b6c76ad334f260e956db66dfc26292877f5e7
3bdallah <abdallahabdelhady777@gmail.com>
2026-01-08 23:12:50 +0200

    extract function to get score when scores of players are equal

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -24,21 +24,7 @@ export class TennisGame1 implements TennisGame {
     let score: string = '';
     let tempScore: number = 0;
     if (this.m_score1 === this.m_score2) {
-      switch (this.m_score1) {
-        case 0:
-          score = 'Love-All';
-          break;
-        case 1:
-          score = 'Fifteen-All';
-          break;
-        case 2:
-          score = 'Thirty-All';
-          break;
-        default:
-          score = 'Deuce';
-          break;
-
-      }
+     score = this.getScoreWhenScoreAreEqual();
     }
     else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
       const minusResult: number = this.m_score1 - this.m_score2;
@@ -68,5 +54,18 @@ export class TennisGame1 implements TennisGame {
       }
     }
     return score;
+  }
+
+  private getScoreWhenScoreAreEqual(): string {
+    switch (this.m_score1) {
+        case 0:
+          return 'Love-All';
+        case 1:
+          return 'Fifteen-All';
+        case 2:
+          return 'Thirty-All';
+        default:
+          return 'Deuce';
+      }
   }
 }
