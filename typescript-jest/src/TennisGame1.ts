f711205ff592d4506a62cdf37c7fa46d078abc26
3bdallah <abdallahabdelhady777@gmail.com>
2026-01-09 00:33:32 +0200

    extract get score based on tempScore into function

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -57,22 +57,21 @@ export class TennisGame1 implements TennisGame {
      for (let i = 1; i < 3; i++) {
         if (i === 1) tempScore = this.m_score1;
         else { score += '-'; tempScore = this.m_score2; }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
-        }
+       score+= this.getScoreBasedOnTempScore(tempScore);
       }
       return score;
+  }
+  private getScoreBasedOnTempScore(tempScore: number): string {
+    switch (tempScore) {
+      case 0:
+        return 'Love';
+      case 1:
+        return 'Fifteen';
+      case 2:
+        return 'Thirty';
+      default:
+        return 'Forty';
+    }
   }
 
 }
