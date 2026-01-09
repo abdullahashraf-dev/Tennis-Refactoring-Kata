f546b81fc79476c20c15e763fda85131d0335804
3bdallah <abdallahabdelhady777@gmail.com>
2026-01-10 01:05:14 +0200

    extract code when player score are equal in getScore function

--- typescript-jest/src/TennisGame2.ts
+++ typescript-jest/src/TennisGame2.ts
@@ -17,18 +17,7 @@ export class TennisGame2 implements TennisGame {
   P2point: number = 0;
   getScore(): string {
     let score: string = '';
-    if (this.P1point === this.P2point && this.P1point < 4) {
-      if (this.P1point === 0)
-        score = 'Love';
-      if (this.P1point === 1)
-        score = 'Fifteen';
-      if (this.P1point === 2)
-        score = 'Thirty';
-      score += '-All';
-    }
-    if (this.P1point === this.P2point && this.P1point >= 3)
-      score = 'Deuce';
-
+    if(this.P1point === this.P2point) return this.getScoreWhenPlayersScoreAreEqual();
     if (this.P1point > 0 && this.P2point === 0) {
       if (this.P1point === 1)
         this.P1res = 'Fifteen';
@@ -90,6 +79,18 @@ export class TennisGame2 implements TennisGame {
       score = 'Win for player2';
     }
     return score;
+  }
+  private getScoreWhenPlayersScoreAreEqual(): string {
+    switch (this.P1point) {
+        case 0:
+          return 'Love-All';
+        case 1:
+          return 'Fifteen-All';
+        case 2:
+          return 'Thirty-All';
+        default:
+          return 'Deuce';
+      }
   }
 
   wonPoint(playerName: string): void {
